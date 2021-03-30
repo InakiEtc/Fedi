514500728b60529f04c8abd72759de84402f7f5a
InakiEtc <64444050+InakiEtc@users.noreply.github.com>
2021-03-30 10:34:46 -0300

    aaaaaa

--- TP2/src/sql.js
+++ TP2/src/sql.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'secret'
+  password : 'pelaroot'
 });
  
 connection.connect(function(err) {
