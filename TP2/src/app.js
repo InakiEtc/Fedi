b3b3337c61a0bc1725ee059ce3a3acce04325edd
InakiEtc <64444050+InakiEtc@users.noreply.github.com>
2021-03-30 22:20:56 -0300

    ashei2.0

--- TP2/src/app.js
+++ TP2/src/app.js
@@ -1,3 +1,19 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'pelaroot'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected to mySQL');
+});
+
 const express = require('express');
 const app = express();
 const port = 3000;
