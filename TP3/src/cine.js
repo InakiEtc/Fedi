463ca9403c0171b00d93cb2ee3996721897e0b61
Iñaki <alumno21.etchegoyen.iniaki@ipm.edu.ar>
2021-05-17 11:35:19 -0300

    aas

---
+++ TP3/src/cine.js
@@ -0,0 +1,145 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var bodyparser = require("body-parser");
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'alumnoipm',
+    database: 'cine'
+});
+var cluster = require('cluster');
+pool.getConnection(function (err, con) {
+    if (err)
+        throw err;
+    console.log('Se conecto correctamente a mySQL');
+});
+if (cluster.isWorker) {
+    process.on('message', function (reservar) {
+        pool.getConnection(function (err, con) {
+            con.beginTransaction(function (err) {
+                if (err)
+                    throw err;
+                con.query("select * from funciones where vigente = 1 and fecha > now() and id = " + reservar.get("idF") + " for update", function (err, result, fields) {
+                    if (err)
+                        throw err;
+                    var funciones = new Array();
+                    result.forEach(function (x) {
+                        funciones.push(x.id);
+                    });
+                    var butacas = JSON.parse(result.butacas_disponibles);
+                    if (funciones == null)
+                        return "La funcion que quiere reservar no existe";
+                    con.query("select * from reservas where usuario = " + reservar.get("idUser") + " for update", function (err, result, fields) {
+                        if (err)
+                            throw err;
+                        var funciones2 = new Array();
+                        result.forEach(function (x) {
+                            funciones2.push(x.funcion);
+                        });
+                        if (funciones2.includes(reservar.get("idF")))
+                            return "Ya sacaste entradas para esta funcion";
+                        if (butacas.length < reservar.get("butacasReservar").length && reservar.get("butacasReservar").length <= 6)
+                            return "No hay butacas suficientes";
+                        var arrayButacasR = [];
+                        for (var i = 0; i < butacas.length; i++) {
+                            for (var j = 0; j < reservar.get("butacasReservar").length; j++) {
+                                if (butacas[i] == reservar.get("butacasReservar")[j]) {
+                                    arrayButacasR.push(reservar.get("butacasReservar")[j]);
+                                    delete butacas[i];
+                                }
+                            }
+                        }
+                        butacas = JSON.stringify(butacas);
+                        var stringButacasR = JSON.stringify(arrayButacasR);
+                        if (butacas.length == 0) {
+                            con.query("update funciones set vigente = 0 and butacas_disponibles = []", function (err, result, fields) {
+                                if (err)
+                                    throw err;
+                            });
+                        }
+                        con.query("update funciones set butacas_disponibles = " + stringButacasR, function (err, result, fields) {
+                            if (err)
+                                throw err;
+                        });
+                        con.query("insert into reservas values(null," + reservar.get("idUser") + "," + reservar.get("idF") + "," + stringButacasR + ")", function (err, result, fields) {
+                            if (err)
+                                throw err;
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+else {
+    var express = require('express');
+    var app = express();
+    var port_1 = 3000;
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+    app.listen(port_1, function () {
+        console.log("Se levanto el server en http://localhost:" + port_1);
+    });
+    app.get('/', function (req, res) {
+        res.send('prende eso?');
+    });
+    app.get('/funciones', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/];
+        });
+    }); });
+    app.post('/:id_funcion/reservar', function (req, res) {
+        console.log(req.body);
+        var idF = req.param('id_funcion');
+        var butacasReservar = req.body.butacas;
+        var idUser = req.body.usuario;
+        var reservar = new Map();
+        reservar.set("idF", idF);
+        reservar.set("butacasReservar", butacasReservar);
+        reservar.set("idUser", idUser);
+        var worker = cluster.fork();
+        worker.send(reservar);
+        worker.on('message', function (result) {
+            res.status(200).send(result);
+        });
+    });
+    app.post('/:id_funcion/cancelar_reserva', function (req, res) {
+    });
+}
--- TP3/src/cine.ts
+++ TP3/src/cine.ts
@@ -1,79 +1,111 @@
-function Conexion(){
-  var mysql      = require('mysql');
-  var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'pelaroot',
-  database : 'cine'
-  });
+import * as bodyparser from 'body-parser';    
 
-  return connection;
-}
+const mysql = require('mysql');
+const pool = mysql.createPool({
+host     : 'localhost',
+user     : 'root',
+password : 'alumnoipm',
+database : 'cine'
+});
+const cluster = require('cluster');
 
-Conexion().connect(function(err) {
+pool.getConnection(function(err,con) {
   if (err) throw err;
   console.log('Se conecto correctamente a mySQL');
 });
 
-const cluster = require('cluster');
-
-const express = require('express');
-const app = express();
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Se levanto el server en http://localhost:${port}`);
-})
-
-app.get('/', (req, res) => {
-  res.send('prende eso?');
-})
-
-function funcionesDisponibles(){ 
-  return new Promise(function (resolve, reject){
-      Conexion().query("select titulo from funciones where vigente = 1 and fecha > now()", function (error, results, fields){
-        if (error) throw error;
-        resolve(results)
-      });
-  });
-}
 
 if(cluster.isWorker){
-  process.on('message', (reserva) => {
-      Conexion().beginTransaction(function(err){
+  process.on('message', (reservar) => {
+    pool.getConnection(function(err,con){
+      con.beginTransaction(function(err){
           if (err) throw err;
-          Conexion().query("select * from funciones where vigente = 1 and fecha > now() and id = "+idF, function (err, result, fields) {
+          con.query("select * from funciones where vigente = 1 and fecha > now() and id = "+reservar.get("idF")+" for update", function (err, result, fields) {
             if (err) throw err;
             let funciones = new Array();
             result.forEach(x => {
               funciones.push(x.id);
             });
-            if(funciones.includes(idF) == false) return "La funcion que quiere reservar no existe";
-            Conexion().query("select * from reservas where usuario = "+/*idUsuari*/+" for update", function (err, result, fields) {
+            let butacas = JSON.parse(result.butacas_disponibles);
+            if(funciones == null) return "La funcion que quiere reservar no existe";
+            con.query("select * from reservas where usuario = "+reservar.get("idUser")+" for update", function (err, result, fields) {
               if (err) throw err;
               let funciones2 = new Array();
               result.forEach(x => {
                 funciones2.push(x.funcion);
               });
-              if(funciones2.includes(idF)) return "Ya sacaste entradas para esta funcion";
-
+              if(funciones2.includes(reservar.get("idF"))) return "Ya sacaste entradas para esta funcion";
+              if(butacas.length < reservar.get("butacasReservar").length && reservar.get("butacasReservar").length <= 6)return "No hay butacas suficientes";
+              let arrayButacasR = [];
+              for (let i = 0; i < butacas.length; i++) {
+                for (let j = 0; j < reservar.get("butacasReservar").length; j++) {
+                  if (butacas[i] == reservar.get("butacasReservar")[j]){
+                    arrayButacasR.push(reservar.get("butacasReservar")[j]);
+                    delete butacas[i];
+                  }
+                }
+              }
+              butacas = JSON.stringify(butacas);
+              let stringButacasR = JSON.stringify(arrayButacasR)
+              if(butacas.length == 0){
+                con.query("update funciones set vigente = 0 and butacas_disponibles = []", function (err, result, fields) {
+                  if (err) throw err;
+                });  
+              }
+              con.query("update funciones set butacas_disponibles = "+stringButacasR, function (err, result, fields) {
+                if (err) throw err;
+              });  
+              con.query("insert into reservas values(null,"+reservar.get("idUser")+","+reservar.get("idF")+","+stringButacasR+")", function (err, result, fields) {
+                if (err) throw err;
+              });  
             });
-
           });
       });
+    })
   });
 }
+else{
+  const express = require('express');
+  const app = express();
+  const port = 3000;
 
-app.get('/funciones', async (req, res) => {
-  let funciones = await funcionesDisponibles();
-  res.json(funciones);
-})
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
 
-app.post('/:id_funcion/reservar', (req, res) => {
-  var idF = req.param('id_funcion');
-    
-})
+  app.listen(port, () => {
+    console.log(`Se levanto el server en http://localhost:${port}`);
+  })
 
-app.post('/:id_funcion/cancelar_reserva', (req, res) => {
-    
-})
+  app.get('/', (req, res) => {
+    res.send('prende eso?');
+  })
+
+  app.get('/funciones', async (req, res) => {
+
+  })
+  
+  app.post('/:id_funcion/reservar', (req, res) => {
+    console.log(req.body)
+    let idF = req.param('id_funcion');
+    let butacasReservar = req.body.butacas;
+    let idUser = req.body.usuario;
+
+    let reservar = new Map();
+    reservar.set(idF, idF);
+    reservar.set(butacasReservar, butacasReservar);
+    reservar.set("idUser", idUser);
+
+    const worker = cluster.fork();
+    worker.send(reservar);
+    worker.on('message', (result) => {
+         res.status(200).send(result);
+    });
+      
+  })
+  
+  app.post('/:id_funcion/cancelar_reserva', (req, res) => {
+      
+  })
+}
+
 
